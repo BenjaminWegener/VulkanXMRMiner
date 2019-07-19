14d06b4ec3c4d9d18a3e10cf15b0182821aedffe
@BenjaminWegener <wegeneredv@gmail.com>
2019-07-19 11:20:17 +0200

    Add files via upload

---
+++ amdNaviOptimization/Miner.bat
@@ -0,0 +1,8 @@
+@echo off
+cls
+setx GPU_MAX_HEAP_SIZE 90	
+setx GPU_MAX_USE_SYNC_OBJECTS 1	
+setx GPU_SINGLE_ALLOC_PERCENT 90	
+setx GPU_MAX_ALLOC_PERCENT 90	
+setx GPU_MAX_SINGLE_ALLOC_PERCENT 90	
+start /low Miner.exe
